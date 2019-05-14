96e156288261479036fe9405d40e86660979e39b
Thananan <59160658@go.buu.ac.th>
2019-05-14 17:00:20 +0700

    14 May 2019 01:40 AM

--- app/src/main/java/com/example/hrlite/MainActivity.kt
+++ app/src/main/java/com/example/hrlite/MainActivity.kt
@@ -2,11 +2,19 @@ package com.example.hrlite
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.Window
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Hide the status bar.
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+        // Remember that you should never show the action bar if the
+        // status bar is hidden, so hide that too if necessary.
+        actionBar?.hide()
     }
 }
